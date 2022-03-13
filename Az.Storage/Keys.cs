f11bf66994c3541301e89d2daca03e4ee67e01d4
Sajin <code@saj.in>
2022-03-13 19:19:12 +0530

    added table name override to AzDataServiceBase

--- Az.Storage/Keys.cs
+++ Az.Storage/Keys.cs
@@ -5,18 +5,21 @@
     public static class Keys
     {
         private static DateTime SINCE = new DateTime(2007, 9, 3, 0, 0, 0, DateTimeKind.Utc);
+        private static DateTime TILL = new DateTime(2107, 9, 3, 0, 0, 0, DateTimeKind.Utc);
 
         public static string InstaSeconds(int offset = 0) => Insta(offset, "yyyyMMddHHmmss");
         public static string InstaMinutes(int offset = 0) => Insta(offset, "yyyyMMddHHmm");
         public static string InstaHour(int offset = 0) => Insta(offset, "yyyyMMddHH");
         public static string InstaDay(int offset = 0) => Insta(offset, "yyyyMMdd");
         public static string InstaMonth(int offset = 0) => Insta(offset, "yyyyMM");
-        public static string ForwardKeyLoRes(int offset = 0) => Since(offset);
-        public static string ForwardKeyHiRes(int offset = 0) => Since(offset, true);
+        public static string IncreasingKeyLoRes(int offset = 0) => Interval(SINCE, offset).ToString();
+        public static string IncreasingKeyHiRes(int offset = 0) => Interval(SINCE, offset, true).ToString();
+        public static string DecreasingKeyLoRes(int offset = 0) => Interval(TILL, offset).ToString();
+        public static string DecreasingKeyHiRes(int offset = 0) => Interval(TILL, offset, true).ToString();
 
         #region Internal Helpers
         private static string Insta(int offset, string format) => OffsetTime(offset).ToString(format);
-        private static string Since(int offset, bool hi = false) => (hi ? (SINCE - OffsetTime(offset)).Ticks : Math.Floor((SINCE - OffsetTime(offset)).TotalMinutes)).ToString();
+        private static double Interval(DateTime from, int offset, bool hi = false) => Math.Abs(hi ? (OffsetTime(offset) - from).TotalMilliseconds : Math.Floor((OffsetTime(offset) - from).TotalMinutes));
         private static DateTime OffsetTime(int offset) => DateTime.UtcNow.AddMinutes(offset);
         #endregion
     }
--- Az.Storage/Service/AzDataServiceBase.cs
+++ Az.Storage/Service/AzDataServiceBase.cs
@@ -7,6 +7,7 @@
 
     public class AzDataServiceBase<T> : IAzDataService<T> where T : ITableEntity, new()
     {
+        protected readonly string _table;
         protected readonly AzureStorageContext _context;
         protected int _split = 0;
 
@@ -14,34 +15,35 @@
         /// Creates an instance acting upon the supplied <c>context</c>
         /// </summary>
         /// <param name="context">The underlying <c>AzureStorageContext</c></param>
-        public AzDataServiceBase(AzureStorageContext context) => _context = context;
+        /// <param name="table">Optional. Table name, if different from T name.</c></param>
+        public AzDataServiceBase(AzureStorageContext context, string table = null) => (_context, _table) = (context, table);
 
         /// <inheritdoc/>
         public virtual async Task<List<T>> GetAll() =>
-            await _context.GetTable<T>();
+            string.IsNullOrEmpty(_table) ? await _context.GetTable<T>() : await _context.GetTable<T>(_table);
 
         /// <inheritdoc/>
         public virtual async Task<List<T>> GetSet(string id) =>
-            await _context.GetPartition<T>(id);
+            string.IsNullOrEmpty(_table) ? await _context.GetPartition<T>(id) : await _context.GetPartition<T>(_table, id);
 
         /// <inheritdoc/>
         public virtual async Task<T> GetOne(string id)
         {
             var keys = _split == 0 ? id.Split('-') : new string[] { id.Substring(0, _split), id };
             if (keys.Length != 2) throw new ArgumentException("ID is invalid");
-            return await _context.GetRow<T>(keys[0], keys[1]);
+            return string.IsNullOrEmpty(_table) ? await _context.GetRow<T>(keys[0], keys[1]) : await _context.GetRow<T>(_table, keys[0], keys[1]);
         }
 
         /// <inheritdoc/>
         public virtual async Task<bool> Create(T obj) =>
-            await _context.Create<T>(obj);
+            string.IsNullOrEmpty(_table) ? await _context.Create<T>(obj) : await _context.Create<T>(_table, obj);
 
         /// <inheritdoc/>
         public virtual async Task<bool> Delete(string id) =>
-            await _context.Delete<T>(await GetOne(id));
+            string.IsNullOrEmpty(_table) ? await _context.Delete<T>(await GetOne(id)) : await _context.Delete<T>(_table, await GetOne(id));
 
         /// <inheritdoc/>
         public virtual async Task<bool> Update(T obj) =>
-            await _context.Update<T>(obj);
+            string.IsNullOrEmpty(_table) ? await _context.Update<T>(obj) : await _context.Update<T>(_table, obj);
     }
 }

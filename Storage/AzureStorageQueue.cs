760bbc698053811f284e74e15b7e32894675893a
Sajin <code@saj.in>
2025-08-26 08:49:02 +0530

    2.2.2

--- Storage/AzureStorageQueue.cs
+++ Storage/AzureStorageQueue.cs
@@ -2,14 +2,13 @@
 
 public partial class AzureStorageContext
 {
-    public void WriteToQueue(string queueName, string message)
+    public async Task Send(string queueName, string message)
     {
         var queue = Queue(queueName);
         if (_createMissing) queue.CreateIfNotExists();
-        queue.SendMessage(Base64Encode(message));
+        await queue.SendMessageAsync(Base64Encode(message));
     }
 
     private static string Base64Encode(string plainText)
         => Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(plainText));
 }
-
